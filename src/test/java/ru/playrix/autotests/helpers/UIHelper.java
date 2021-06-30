91bc7de8dfdc525dc23ddad850c2107403e5c9f8
Pulsaronline <beslite@gmail.com>
2021-06-30 23:46:05 +0300

    Code refactoring

--- src/test/java/ru/playrix/autotests/helpers/UIHelper.java
+++ src/test/java/ru/playrix/autotests/helpers/UIHelper.java
@@ -1,11 +1,13 @@
 package ru.playrix.autotests.helpers;
 
+import ru.playrix.autotests.tests.UITests;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
-public class UIHelper {
+public class UIHelper extends UITests {
     public static void checkLocale(){
         if($(".header__menu.main-menu").shouldHave(text("RU")).isDisplayed()) {
             $(byText("RU")).click();
