7b2043618d394a304ded67e0894bc9a73297dbfb
Pulsaronline <beslite@gmail.com>
2021-06-30 13:07:50 +0300

    uploadCV() refactoring 5

--- src/test/java/cloud/autotests/tests/cv/pages/ui/CvPage.java
+++ src/test/java/cloud/autotests/tests/cv/pages/ui/CvPage.java
@@ -126,7 +126,7 @@ public class CvPage {
 
     public static void uploadCV(){
         //File file = new File("/src/test/resources/cv/" + filename);
-        $(byName(uploadCVFileAttributeSelector)).uploadFromClasspath("/src/test/resources/cv/" + filename);;
+        $(byName(uploadCVFileAttributeSelector)).uploadFromClasspath("cv/" + filename);;
 //        $(byAttribute(uploadCVFileAttributeSelector, uploadCVFileAttributeValue))
 //                .uploadFromClasspath("cv/" + filename);
     }
